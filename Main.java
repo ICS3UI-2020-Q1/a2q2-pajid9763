f6871a6785768392ca2228f17b4c79ae2312caef
pajid9763 <pajid9763@wrdsb.ca>
2020-09-14 15:51:56 +0000

    finished program to find bigger number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * 
+ * @author Darian Pajic
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  Scanner input = new Scanner(System.in);
+
+  // ask for the integers
+  System.out.println("Please enter an integer:");
+  int a = input.nextInt();
+  System.out.println("Please enter another integer:");
+  int b = input.nextInt();
+
+  // determine which number is bigger or if they are the same
+  if (a > b){
+    System.out.println("The biggest number is " + a + ".");
+  } else if(a == b){
+    System.out.println("There is no biggest number.");
+  }else{ 
+    System.out.println("The biggest number is " + b + ".");
+  
+  }
+
     
   }
 }
